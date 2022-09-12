8171bb8a71194c28510387a6468042ccf491e8d8
attilak125 <attila.k125@gmail.com>
2022-09-12 10:42:09 +0200

    Rabits

---
+++ src/main/java/com/example/rabits/RabitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabitsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RabitsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/rabits/RabitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabitsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
